2d05383e9ef9bac7aa8bc8242e9766614b08c7e8
hithesh svsk <8svskhd@gmail.com>
2021-01-25 15:20:23 +0530

    indexchanges4

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,9 @@ export default function Home() {
     <Inav/>
     <div className="INH">
     <h1 className="IHT">SevenHills Tirupati</h1>
+    <div className="IHSUB">
     <h2>Front page of Tirupati and Tirumala</h2>
+    </div>
     </div>
     </div>
     {/* cards */}
