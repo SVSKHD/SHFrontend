a1784b1e13ec4220f4e702ca0c9033b341187321
hithesh svsk <8svskhd@gmail.com>
2021-01-25 10:43:36 +0530

    index changes3

--- Components/Layout/Inav.js
+++ Components/Layout/Inav.js
@@ -7,12 +7,13 @@ import {
   Nav,
   NavItem,
   NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
+  Button,
+  Card,
+  ButtonGroup,
 } from 'reactstrap';
+import {isAuth, signout} from "../../actions/auth"
+import Link from "next/link"
+import  Router  from 'next/router';
 
 const Inav = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,7 +22,7 @@ const Inav = (props) => {
 
   return (
     <div className="Navbar">
-      <Navbar dark expand="md">
+      <Navbar light expand="md">
         
         <NavbarBrand href="/"><img height="100" width="100"
            className="rounded-circle img-thumbnail border-warning "
@@ -34,30 +35,67 @@ const Inav = (props) => {
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink href="/components/">Blogs</NavLink>
+              <NavLink href="/Blogs">Blogs</NavLink>
             </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
+            
+            
+
+
+           
+
+           
           </Nav>
-       
+           
+          <Nav className="mr-auto" navbar>
+          {!isAuth() && (
+            <React.Fragment>
+            <Card>
+            <ButtonGroup>
+            <Button color="warning">
+              <NavLink href="/signup"><b>Signup</b></NavLink>
+            </Button>
+
+            <Button color="success">
+              <NavLink href="/signin"><b>Signin</b></NavLink>
+            </Button>
+            </ButtonGroup>
+            </Card>
+            </React.Fragment>
+            )}
+
+            {isAuth() && isAuth().role === 0 && (
+                    
+                    <React.Fragment>
+                      <Link href="/user">
+                      <Button color ="light"><b>{`${isAuth().name}'s Dashboard`}</b></Button>
+                      </Link>
+                    </React.Fragment>
+            )}
+
+            {isAuth() && isAuth().role === 1 && (
+                    
+                    <React.Fragment>
+                      <Link href="/user">
+                      <Button color ="light"><b>{`${isAuth().name}'s Dashboard`}</b></Button>
+                      </Link>
+                    </React.Fragment>
+            )}
+            
+            {isAuth() && (
+                    
+                    <React.Fragment>
+                      <Button 
+                      style={{ cursor: 'pointer' }}
+                      onClick={()=>signout(()=>Router.replace(`/`))} 
+                      className="ISH" 
+                      color ="danger">
+                          <b>Signout</b>
+                    </Button>
+                    </React.Fragment>
+            )}
+
+          </Nav>
+
         </Collapse>
       </Navbar>
     </div>
